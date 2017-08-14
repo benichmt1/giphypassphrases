a6c6bc56209bfaa31a96f004ee9c14eef0ff9817
Michael Benich <benichmt1@protonmail.com>
2017-08-14 15:46:12 -0400

    pep8 reeeeee

--- giphypass.py
+++ giphypass.py
@@ -1,10 +1,12 @@
-import argparse, safygiphy, sys
+import argparse
+import safygiphy
+import sys
 from colorama import init, Fore
 init()
 parser = argparse.ArgumentParser()
-parser.add_argument("keyword", help="The keyword you wish to base your password off of")
+parser.add_argument("keyword", help="Base password keyword")
 args = parser.parse_args()
-g= safygiphy.Giphy()
+g = safygiphy.Giphy()
 
 r = g.random(tag=args.keyword)
 if not r[u'data']:
@@ -13,7 +15,7 @@ if not r[u'data']:
 t = r[u'data'][u'url']
 print (Fore.BLUE + "URL Link:")
 print (Fore.WHITE + t)
-full = t.rsplit('/',1)[-1]
+full = t.rsplit('/', 1)[-1]
 if (len(full) < 19):
         print (Fore.GREEN + "ID")
         print (Fore.WHITE + full)
@@ -21,7 +23,6 @@ else:
         print (Fore.GREEN + "Most complexity")
         print (Fore.WHITE + full)
         print (Fore.YELLOW + "Phrase Idea")
-        print (Fore.WHITE + full.rsplit('-',1)[0])
+        print (Fore.WHITE + full.rsplit('-', 1)[0])
         print (Fore.CYAN + "ID Tag")
-        print (Fore.WHITE + full.rsplit('-',1)[-1])
-
+print (Fore.WHITE + full.rsplit('-', 1)[-1])
